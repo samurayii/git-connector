f94990737e276d061ac8ce7efc79140403574e69
Alexand <samurayiii1986@gmail.com>
2021-04-15 02:10:57 +0300

    0.2.11

--- src/lib/entry.ts
+++ src/lib/entry.ts
@@ -11,15 +11,22 @@ type TPackage = {
 
 const findPkg = (): TPackage => {
 
+    const cwd_pkg_full_path = path.resolve(process.cwd(), "package.json");
     const dirname_pkg_full_path = path.resolve(__dirname, "package.json");
     const app_pkg_full_path = path.resolve(path.dirname(process.argv[1]), "package.json");
+    const require_pkg_full_path = path.resolve(path.dirname(require.main.filename), "package.json"); 
 
     if (fs.existsSync(dirname_pkg_full_path) === true) {
         return <TPackage>JSON.parse(fs.readFileSync(dirname_pkg_full_path).toString());
     }
-    
     if (fs.existsSync(app_pkg_full_path) === true) {
         return <TPackage>JSON.parse(fs.readFileSync(app_pkg_full_path).toString());
+    }
+    if (fs.existsSync(require_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(require_pkg_full_path).toString());
+    }   
+    if (fs.existsSync(cwd_pkg_full_path) === true) {
+        return <TPackage>JSON.parse(fs.readFileSync(cwd_pkg_full_path).toString());
     }
 
 };
